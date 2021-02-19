7ae376f8d25cfa4510f51b4f40f08d24f7b4220c
mehmetlevent <41065086+mehmetlevent@users.noreply.github.com>
2021-02-19 15:26:28 +0300

    Add files via upload

--- src/components/app.js
+++ src/components/app.js
@@ -1,21 +1,35 @@
-import ReactDOM from "react-dom";
+import ReactDom from "react-dom";
 import React from 'react';
 
 class App extends React.Component {
-    constructor(props) {
+    constructor (props) {
         super(props);
         this.state = {}
+        this.authFunc = this.authFunc.bind(this);
+    }
+    authFunc(){
+    const authData = {data: "Auth benim sitemde"};
+       if (WavesKeeper) {
+            WavesKeeper.auth(authData)
+            .then(auth => {
+                console.log(auth);
+            }).catch(error => {
+                console.error(error);
+
+            })
+        } else {
+            alert("to auth WavesKeeper should be installed.(önce yüklemelisin)");
+        }
     }
     render() {
         return (
-            <div className="container">
-                <input className="btn btn-primary" type="submit" value="Alert" onClick={() => {alert("Alert button onClick");}}/>
+            <div className ="container">
+                <input className="btn btn-primary" type="submit" value="Auth" onClick={this.authFunc}/>
             </div>
         )
     }
 }
-
-const app = document.getElementById('app');
-if(app) {
-    ReactDOM.render(<App/>, app);
+const app= document.getElementById('app');
+if(app){
+    ReactDom.render(<App/>, app);
 }
